280ada5254ef9bf36c53d0f4dccb5768d871ca3b
ywxt <ywxtcwh@qq.com>
2018-12-09 14:51:11 +0800

    修改了一下项目

---
+++ CLesson/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace CLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.WriteLine("请输入替换的路径:");
+            var path = Console.ReadLine();
+            Console.WriteLine("请输入需要被替换的字符串:");
+            var oldValue = Console.ReadLine();
+            Console.WriteLine("请输入新字符串:");
+            var newValue = Console.ReadLine();
+            Console.WriteLine("请输入字符编码(回车默认Windows为GBK，Linux和macOS为UTF-8，一般不需要改):");
+            var encoding = Console.ReadLine();
+            try
+            {
+                var e = string.IsNullOrWhiteSpace(encoding) ? Encoding.GetEncoding(0) : Encoding.GetEncoding(encoding);
+                oldValue = !string.IsNullOrWhiteSpace(oldValue) ? oldValue : "崔文辉 2018117504";
+                newValue = !string.IsNullOrWhiteSpace(newValue) ? newValue : "孔德贵 2018117528";
+                var program = new CProgram
+                {
+                    Encoding = e,
+                    OldValue = e.GetBytes(oldValue),
+                    NewValue = e.GetBytes(newValue)
+                };
+                program.ReplaceAll(path);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("输入的编码有误");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
